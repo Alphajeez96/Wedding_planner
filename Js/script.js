f71ec1ff3150b07be9bb418da0b5154888655ca4
Prince <chimeremezechukwudire@gmail.com>
2020-07-10 19:05:59 +0100

    contact section added

--- Js/script.js
+++ Js/script.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
         responsive: [{
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true
                 }
@@ -20,8 +20,8 @@ $(document).ready(function() {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
